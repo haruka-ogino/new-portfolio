aaa1e2c405e1779da92b049b28b09e18ac82d267
Haruka Ogino <harukariq@gmail.com>
2024-06-14 20:29:20 +1200

    create projects component

---
+++ components/Projects.tsx
@@ -0,0 +1,11 @@
+import '../styles/about.css'
+
+export default function Projects() {
+  return (
+    <section className="projects">
+      <div>
+        <h1>My Projects</h1> =
+      </div>
+    </section>
+  )
+}
