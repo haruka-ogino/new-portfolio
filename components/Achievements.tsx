5cbb0788322d16e51f638761a0b252659a9f6bff
Haruka Ogino <harukariq@gmail.com>
2024-06-29 13:02:56 +1200

    add WEN to achievements section

--- components/Achievements.tsx
+++ components/Achievements.tsx
@@ -3,6 +3,23 @@ export default function Achievements() {
     <>
       <div>
         <h1>Extra-curricular Activities & Achievements</h1>{' '}
+        <h2>Women in Engineering Network (WEN) 2017 – 2021</h2>
+        <ul>
+          <li>
+            - Mentored students as part of the Industry Chapter Mentoring
+            program, successfully helping them land their first role in the
+            civil engineering industry.
+          </li>
+          <li>
+            - Received mentorship from older students and working professionals
+            on landing my first role and professional development as a woman in
+            STEM.
+          </li>
+        </ul>
+        <h2>Unleash Space, Newsroom Writer 2019</h2>
+        Authored weekly articles covering events, upcoming activities, and ideas
+        created at Unleash Space. Provided support during events as a member of
+        the Unleash Team.
       </div>
     </>
   )
