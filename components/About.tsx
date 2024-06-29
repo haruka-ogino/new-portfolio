8eaffdcc56ea9f39965e9308c795100b3d41329a
Haruka Ogino <harukariq@gmail.com>
2024-06-29 19:45:44 +1200

    responsive styling

--- components/About.tsx
+++ components/About.tsx
@@ -19,32 +19,36 @@ export default function About() {
         <p>Full-stack Developer</p>
       </section>
       <div className="about-me">
-        <p>
-          I am a full-stack developer dedicated to personal and professional
-          growth. Having transitioned from civil engineering, I have discovered
-          a deep passion for coding. The ability to solve problems and bring
-          ideas to life with just a few keystrokes fuels my creativity and
-          ambition. I am currently seeking a software development role where I
-          can contribute to dynamic teams, further develop my expertise in
-          full-stack development, and continue to inspire and encourage women to
-          pursue careers in STEM, especially in software engineering.{' '}
-        </p>
         <img src="/headshot.png" />
+        <div>
+          <p>
+            I am a full-stack developer dedicated to personal and professional
+            growth. Having transitioned from civil engineering, I have
+            discovered a deep passion for coding. The ability to solve problems
+            and bring ideas to life with just a few keystrokes fuels my
+            creativity and ambition. I am currently seeking a software
+            development role where I can contribute to dynamic teams, further
+            develop my expertise in full-stack development, and continue to
+            inspire and encourage women to pursue careers in STEM, especially in
+            software engineering.{' '}
+          </p>
+
+          <section className="my-links">
+            {links.map((link, i) => (
+              <a key={i} href={link.link}>
+                {link.name === 'LinkedIn' && (
+                  <FaLinkedin size={35} style={{ marginBottom: '-2px' }} />
+                )}
+                {link.name === 'GitHub' && (
+                  <FaGithubSquare size={35} style={{ marginBottom: '-2px' }} />
+                )}
+                {link.name === 'CV' && <FaFileAlt size={30} />}
+                {/* {link.name} */}
+              </a>
+            ))}
+          </section>
+        </div>
       </div>
-      <section className="my-links">
-        {links.map((link, i) => (
-          <a key={i} href={link.link}>
-            {link.name === 'LinkedIn' && (
-              <FaLinkedin size={35} style={{ marginBottom: '-2px' }} />
-            )}
-            {link.name === 'GitHub' && (
-              <FaGithubSquare size={35} style={{ marginBottom: '-2px' }} />
-            )}
-            {link.name === 'CV' && <FaFileAlt size={30} />}
-            {/* {link.name} */}
-          </a>
-        ))}
-      </section>
     </>
   )
 }
