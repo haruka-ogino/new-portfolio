eaf8980cb0ad7651e29be123214553dc9e266f87
Haruka Ogino <harukariq@gmail.com>
2024-06-30 16:13:07 +1200

    add links to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,36 @@
+import { FaLinkedin, FaGithubSquare, FaFileAlt } from 'react-icons/fa'
+import '../styles/footer.css'
 export default function Footer() {
+  const links = [
+    {
+      name: 'LinkedIn',
+      link: 'https://nz.linkedin.com/in/haruka-ogino-769540148',
+    },
+    { name: 'GitHub', link: 'https://github.com/haruka-ogino' },
+    { name: 'CV', link: '/Haruka-Ogino-CV.pdf' },
+  ]
   return (
     <>
       <footer>
-        <h1>Haruka Ogino</h1>
-        <h1>Haruka Ogino</h1>
+        <section className="footer-links">
+          {links.map((link, i) => (
+            <a
+              key={i}
+              href={link.link}
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                marginRight: '15px',
+              }}
+            >
+              {link.name === 'LinkedIn' && <FaLinkedin size={28} />}
+              {link.name === 'GitHub' && <FaGithubSquare size={28} />}
+              {link.name === 'CV' && <FaFileAlt size={25} />}
+              <p style={{ fontSize: '18px' }}>{link.name}</p>
+            </a>
+          ))}
+        </section>
+        {/* <h1>Haruka Ogino</h1> */}
       </footer>
     </>
   )
