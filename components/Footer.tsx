0dfd3eb9b2cc432085ff822ce1bc198cb4d48879
Haruka Ogino <harukariq@gmail.com>
2024-06-30 18:33:05 +1200

    style portfolio tech stack

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,59 +17,62 @@ export default function Footer() {
   return (
     <>
       <footer>
-        <section className="contact-info">
-          <h3>Haruka Ogino</h3>
-          <h4>Full-stack Developer</h4>
-        </section>
-        <section className="footer-links">
-          <a
-            href="mailto:harukariq@gmail.com"
-            target="_blank"
-            rel="noreferrer"
-            style={{ marginLeft: '2px' }}
-          >
-            <FaEnvelope size={24} />
-            <p style={{ marginLeft: '2px' }}>{'harukariq@gmail.com'}</p>
-          </a>
-          {links.map((link, i) => (
-            <a key={i} href={link.link}>
-              {link.name === 'LinkedIn' && <FaLinkedin size={28} />}
-              {link.name === 'GitHub' && <FaGithubSquare size={28} />}
-              {link.name === 'CV' && (
-                <FaFileAlt
-                  size={25}
-                  style={{ marginLeft: '1.5px', marginRight: '1.5px' }}
-                />
-              )}
-              <p>{link.name}</p>
+        <div>
+          <section className="name">
+            <h3>Haruka Ogino</h3>
+            <h4>Full-stack Developer</h4>
+          </section>
+          <section className="footer-links">
+            <a
+              href="mailto:harukariq@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+              style={{ marginLeft: '2px' }}
+            >
+              <FaEnvelope size={24} />
+              <p style={{ marginLeft: '2px' }}>harukariq@gmail.com</p>
             </a>
-          ))}
-        </section>
-
-        <section>
-          <h2>This portfolio was built using:</h2>
+            {links.map((link, i) => (
+              <a key={i} href={link.link}>
+                {link.name === 'LinkedIn' && <FaLinkedin size={28} />}
+                {link.name === 'GitHub' && <FaGithubSquare size={28} />}
+                {link.name === 'CV' && (
+                  <FaFileAlt
+                    size={25}
+                    style={{ marginLeft: '1.5px', marginRight: '1.5px' }}
+                  />
+                )}
+                <p>{link.name}</p>
+              </a>
+            ))}
+          </section>
+        </div>
+        <section style={{ margin: '0em 2em' }}>
+          <h4 style={{ marginBottom: '1em' }}>
+            This portfolio was built using:
+          </h4>
           <ul>
             <li>
               <img
-                src="https://img.shields.io/badge/React-black?style=fflat-square&logo=react"
+                src="https://img.shields.io/badge/React-224756?style=fflat-square&logo=react"
                 alt="react js banner"
               />
             </li>
             <li>
               <img
-                src="https://img.shields.io/badge/Tailwind_CSS-black?style=fflat-square&logo=tailwind-css&logoColor=38B2AC"
+                src="https://img.shields.io/badge/Tailwind_CSS-224756?style=fflat-square&logo=tailwind-css&logoColor=38B2AC"
                 alt="tailwind banner"
               />
             </li>
             <li>
               <img
-                src="https://img.shields.io/badge/Next.js-black?style=fflat-square&logo=nextdotjs&logoColor=white"
+                src="https://img.shields.io/badge/Next.js-224756?style=fflat-square&logo=nextdotjs&logoColor=white"
                 alt="next js banner"
               />
             </li>
             <li>
               <img
-                src="https://img.shields.io/badge/MongoDB-black?style=fflat-square&logo=mongodb"
+                src="https://img.shields.io/badge/MongoDB-224756?style=fflat-square&logo=mongodb"
                 alt="mongoDB banner"
               />
             </li>
