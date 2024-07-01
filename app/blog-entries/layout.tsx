48793a37a2ad1ab38c7e125c99ef61b2691b494c
Haruka Ogino <harukariq@gmail.com>
2024-07-01 16:53:49 +1200

    create layout with nav

--- app/blog-entries/layout.tsx
+++ app/blog-entries/layout.tsx
@@ -1,3 +1,17 @@
+import Link from 'next/link'
+import '../../styles/nav.css'
+import '../../styles/home.css'
+
 export default function layout({ children }: any) {
-  return <>{children}</>
+  return (
+    <>
+      <nav className="nav hidden">
+        <button className="buttons">
+          <Link href="/">← Back to main page</Link>
+        </button>
+      </nav>
+      <h1>My Blog Entries</h1>
+      {children}
+    </>
+  )
 }
