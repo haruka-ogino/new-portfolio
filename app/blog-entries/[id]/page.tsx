348965da034b3cbf5621644ab8a88295e3445fab
Haruka Ogino <harukariq@gmail.com>
2024-07-04 21:58:12 +1200

    dynamically render blog info

--- app/blog-entries/[id]/page.tsx
+++ app/blog-entries/[id]/page.tsx
@@ -6,7 +6,17 @@ const BlogPost = ({ params }: { params: { id: string } }) => {
   return (
     <div>
       <h1>{blogEntries[blogId].title}</h1>
-      <p>Content of blog post with ID: {blogId}</p>
+      <h4 style={{ marginLeft: '2em' }}>
+        <em>posted on {blogEntries[blogId].date}</em>
+      </h4>
+      <p
+        dangerouslySetInnerHTML={{
+          __html: blogEntries[blogId].text.replace(/\n/g, '<br>'),
+        }}
+      />
+      {blogEntries[blogId].images?.map((image, i) => (
+        <img src={image.link} alt={image.description} />
+      ))}
     </div>
   )
 }
