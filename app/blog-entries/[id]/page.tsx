2d2e0bc57312794f47cc35b70b454511846f9a3b
Haruka Ogino <harukariq@gmail.com>
2024-07-04 21:51:24 +1200

    display each blog title on the apge

--- app/blog-entries/[id]/page.tsx
+++ app/blog-entries/[id]/page.tsx
@@ -1,8 +1,12 @@
+import { BlogEntry } from '../../../models/blog'
+import { blogEntries } from '@/public/blogs'
+
 const BlogPost = ({ params }: { params: { id: string } }) => {
+  const blogId = Number(params.id) - 1
   return (
     <div>
-      <h1>Blog Post {params.id}</h1>
-      <p>Content of blog post with ID: {params.id}</p>
+      <h1>{blogEntries[blogId].title}</h1>
+      <p>Content of blog post with ID: {blogId}</p>
     </div>
   )
 }
