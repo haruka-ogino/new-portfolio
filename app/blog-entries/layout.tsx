e9a3b3014ea4953b4108312750c5324db8e709de
Haruka Ogino <harukariq@gmail.com>
2024-07-04 22:36:46 +1200

    make blog nav component

--- app/blog-entries/layout.tsx
+++ app/blog-entries/layout.tsx
@@ -1,18 +1,12 @@
 import Link from 'next/link'
 import '../../styles/nav.css'
 import '../../styles/home.css'
+import BlogNav from '@/components/BlogNav'
 
 export default function layout({ children }: any) {
   return (
     <>
-      <nav className="nav hidden">
-        <button className="buttons">
-          <Link href="/">← Back to main page</Link>
-        </button>
-        <button className="buttons">
-          <Link href="/blog-entries">My Blog Entries</Link>
-        </button>
-      </nav>
+      <BlogNav />
       <div
         style={{
           margin: '0em 2em',
---
+++ components/BlogNav.tsx
@@ -0,0 +1,69 @@
+'use client'
+
+import { RefObject, useRef, useState } from 'react'
+import '../styles/nav.css'
+import { Section } from '@/models/sections'
+import { FaBars } from 'react-icons/fa6'
+import Link from 'next/link'
+
+export default function BlogNav() {
+  const [open, setOpen] = useState(false)
+
+  const closeNav = () => {
+    setOpen(false)
+  }
+
+  const scrollToFooter = () => {
+    const footerElement = document.getElementById('footer')
+    if (footerElement) {
+      footerElement.scrollIntoView({ behavior: 'smooth' })
+    }
+    closeNav()
+  }
+
+  return (
+    <>
+      <nav className="nav hidden">
+        <button className="buttons">
+          <Link href="/">← Back to main page</Link>
+        </button>
+        <button className="buttons">
+          <Link href="/blog-entries">My Blog Entries</Link>
+        </button>
+      </nav>
+      <nav className="small-hidden">
+        {!open ? (
+          <div className="small-nav" onClick={() => setOpen(true)}>
+            <div id="open-icon">
+              <h5>━</h5>
+              <h5>━</h5>
+              <h5>━</h5>
+            </div>
+          </div>
+        ) : (
+          <>
+            <div className="small-nav" id="small-navbar">
+              <div id="close-icon" onClick={closeNav}>
+                <h5>x</h5>
+              </div>
+              <div className="nav-items">
+                <button className="buttons small-buttons" onClick={closeNav}>
+                  <Link href="/">← Back to main page</Link>
+                </button>
+                <button className="buttons small-buttons" onClick={closeNav}>
+                  <Link href="/blog-entries">My Blog Entries</Link>
+                </button>
+                <button
+                  className="buttons small-buttons"
+                  onClick={scrollToFooter}
+                >
+                  About this page
+                </button>
+              </div>
+            </div>
+          </>
+        )}
+      </nav>
+    </>
+  )
+}
