5afa079d786766bc11c0baedc35993b45921fc2a
Haruka Ogino <harukariq@gmail.com>
2024-09-13 13:34:19 +1200

    dyncamically render popup content

--- components/ProjectPopup.tsx
+++ components/ProjectPopup.tsx
@@ -3,19 +3,17 @@ import '../styles/popup.css'
 interface Props {
   setShow: React.Dispatch<React.SetStateAction<boolean>>
   title: string
+  renderContent: JSX.Element
 }
 
-export default function PopUp({ setShow, title }: Props) {
+export default function PopUp({ setShow, title, renderContent }: Props) {
   return (
     <>
       <h2 className="popup-title">{title}</h2>
       <button id="close" onClick={() => setShow(false)}>
         x
       </button>
-      <p>
-        This Project is currently undergoing development and has yet to be
-        deployed.
-      </p>
+      {renderContent}
     </>
   )
 }
